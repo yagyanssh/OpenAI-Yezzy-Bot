1a51d2eb148dfcfea9f3f1ed076572854c4c526c
yagyanssh <yagyansh9999@gmail.com>
2024-05-03 00:30:56 +0530

    Upgraded openai-v

--- openai_example.py
+++ openai_example.py
@@ -1,20 +1,24 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-proj-MvUhkzV1dmyoPea2p0RVT3BlbkFJVe6ybMjEtOY6EYsYa7Cp")
+client = OpenAI(api_key="API_KEY")
 
 # Set up your API key
 
 # Set up the prompt and parameters
 prompt = "What is One Piece?"
-model = "text-davinci-002"
+model = "gpt-3.5-turbo-instruct"
 temperature = 0.7
 max_tokens = 100
 
 # Generate text using OpenAI's GPT-3
-response = client.completions.create(engine=model,
-prompt=prompt,
-temperature=temperature, 
-max_tokens=max_tokens)
+response = client.completions.create(
+    model=model,  # Use 'model' instead of 'engine'
+    prompt=prompt,
+    stream=None,
+    temperature=temperature, 
+    max_tokens=max_tokens
+)
+
 
 # Print the generated text
 print(response.choices[0].text.strip())
